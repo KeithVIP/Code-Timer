510a437ff307160ca65fd1e81c062a8a9e5b15ef
Keith <keithvip@gmail.com>
2022-05-30 14:20:13 -0500

    timer

---
+++ questions.js
@@ -0,0 +1,49 @@
+var questions = [
+  {
+    title: "Commonly used Data types do NOT include:",
+    choices: ["Characters", "Strings", "Booleans", "Alerts"],
+    answer: "Alerts"
+  },
+  {
+    title: "The condition in an if/else staement is enclosed in:",
+    choices: [
+      "Quotes",
+      "Parenthesis",
+      "Curly Brackets",
+      "None of the above"
+    ],
+    answer: "Parenthesis"
+  },
+
+  {
+    title: "Arrays in Javascript can be used to store_________:",
+    choices: [
+      "Numbers and strings",
+      "Other arrays",
+      "Booleans",
+      "All of the above"
+    ],
+    answer: "All of the above"
+  },
+  {
+    title:
+      "String valles must be enclosed within __________ when being assigned to variables.",
+    choices: [
+      "Commas",
+      "Curly brackets",
+      "quotes",
+      "parenthesis"
+    ],
+    answer: "curly brackets"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debuggers is:",
+    choices: [
+      "Javascript",
+      "terminal/bash",
+      "for loops",
+      "console.log"
+    ],
+    answer: "for loops"
+  },
+];
---
+++ script.js
@@ -0,0 +1,130 @@
+var currentQuestionIndex = 0;
+var timer = questions.length * 15;
+var timerId;
+
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#timer");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var returnEl = document.querySelector("#return");
+
+
+function startQuiz() {
+  var startScreenEl = document.getElementById("start");
+  startScreenEl.setAttribute("class", "hide");
+
+  questionsEl.removeAttribute("class");
+
+  timerId = setInterval(tick, 1000);
+
+  timerEl.textContent = timer;
+
+  getQuestion();
+}
+
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  choicesEl.innerHTML = "";
+
+  currentQuestion.choices.forEach(function (choice, i) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    choiceNode.onclick = questionClick;
+
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+function questionClick() {
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    timer -= 15;
+
+    if (timer < 0) {
+      timer = 0;
+    }
+
+    timerEl.textContent = timer;
+    returnEl.textContent = "Wrong!";
+    returnEl.style.color = "red";
+    returnEl.style.fontSize = "500%";
+  } else {
+    returnEl.textContent = "Correct!";
+    returnEl.style.color = "blue";
+    returnEl.style.fontSize = "500%";
+  }
+
+  returnEl.setAttribute("class", "return");
+  setTimeout(function () {
+    returnEl.setAttribute("class", "return hide");
+  }, 1000);
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+function quizEnd() {
+  clearInterval(timerId);
+
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = timer;
+
+  questionsEl.setAttribute("class", "hide");
+}
+
+function tick() {
+  timer--;
+  timerEl.textContent = timer;
+
+  if (timer <= 0) {
+    quizEnd();
+  }
+}
+
+function saveHighscore() {
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    var newScore = {
+      score: timer,
+      initials: initials,
+    };
+
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    window.location.href = "score.html";
+  }
+}
+
+function checkForEnter(event) {
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
